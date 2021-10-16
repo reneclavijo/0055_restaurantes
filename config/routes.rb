d1ea73855ee69cfa666efb02c1b5145a259039a3
rene rodriguez <reneclavijo@live.com>
2021-10-16 11:44:14 -0500

    creando rutas para eliminar un tipo de comida

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
   # Rutas Tipos Comidas
-  get   'tipos_comidas', to: 'tipos_comidas#listar',    as: 'listar_tipos_comida'
-  get   'nuevo_tipo_comida', to: 'tipos_comidas#nuevo', as: 'nuevo_tipo_comida'
+  get      'tipos_comidas', to: 'tipos_comidas#listar',    as: 'listar_tipos_comida'
+  get      'tipos_comidas/nuevo', to: 'tipos_comidas#nuevo', as: 'nuevo_tipo_comida'
 
-  post  'nuevo_tipo_comida', to: 'tipos_comidas#crear'
+  post     'nuevo_tipo_comida', to: 'tipos_comidas#crear'
+  
+  delete   'tipos_comidas/:id', to: 'tipos_comidas#eliminar', as: 'eliminar_tipo_comida'  
 end

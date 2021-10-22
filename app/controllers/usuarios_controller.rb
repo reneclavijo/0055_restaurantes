bbce58a5fdfdc8a9accf302446e1e2045e455a8a
rene rodriguez <reneclavijo@live.com>
2021-10-21 20:38:11 -0500

    agregando el formulario para registrar un usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -3,5 +3,6 @@ class UsuariosController < ApplicationController
     
     # GET /usuarios/nuevo
     def crear
+        @usuario = Usuario.new
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,5 @@ Rails.application.routes.draw do
   # Usuarios
 
   get       'usuarios/nuevo',           to: 'usuarios#crear',         as: 'nuevo_usuario'
+  post      'usuarios',                 to: 'usuarios#guardar',       as: 'usuarios'
 end

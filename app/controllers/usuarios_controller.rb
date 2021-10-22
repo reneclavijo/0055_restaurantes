bdbb3ac4eac44047ef091b923da0fbb18bda4b9d
rene rodriguez <reneclavijo@live.com>
2021-10-22 18:04:36 -0500

    agregando método para guardar un usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -5,4 +5,15 @@ class UsuariosController < ApplicationController
     def crear
         @usuario = Usuario.new
     end
+
+    # POST /usuarios
+    def guardar
+        datos_usuario = params.require(:usuario).permit(:nombre_usuario, :password, :password_confirmation)
+        @usuario = Usuario.new(datos_usuario)
+        if @usuario.save
+            # mostrar un mensaje o vista de confirmación
+        else
+            render :crear
+        end
+    end
 end

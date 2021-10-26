b1eb140c7c7fb4456eba7e7b47a4ccb228726aae
rene rodriguez <reneclavijo@live.com>
2021-10-25 20:22:51 -0500

    configurando todos los modelos del diagrama ER

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,6 +2,16 @@
 class Usuario < ApplicationRecord
     has_secure_password
 
+    has_many :invitaciones
+    # has_many :invitaciones_restaurantes, through: :invitaciones, source: :restaurante
+    # has_many :camino1, through: :invitaciones, source: :restaurante
+    
+    has_many :promociones
+    # has_many :camino2, through: :promociones, source: :restaurante
+
+    has_many :puntajes_platos    
+    has_many :puntajes_restaurantes
+
     validates(:nombre_usuario, presence: true)
     validates(:nombre_usuario, uniqueness: true)
     

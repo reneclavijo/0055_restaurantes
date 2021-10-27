6ec582ede6e9a8abf0b022597b10e304a1bff407
rene rodriguez <reneclavijo@live.com>
2021-10-26 19:11:09 -0500

    agregando el formulario y validaciones para crear restaurantes

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -3,11 +3,20 @@ class RestaurantesController < ApplicationController
 
     # GET /restaurantes/nuevo
     def crear
-        @restaurante = Restaurante.new
+        @restaurante    = Restaurante.new
+        @tipos_comidas  = TipoComida.all 
     end
 
     # POST /restaurantes
     def guardar
-        
+        datos_restaurantes = params.require(:restaurante)
+        .permit(:nombre, :tipo_comida_id)
+        @restaurante = Restaurante.new(datos_restaurantes)
+        if @restaurante.save
+            redirect_to restaurantes_path #TODO: crear la ruta de todos los resturantes
+        else
+            @tipos_comidas = TipoComida.all
+            render :crear
+        end
     end
 end

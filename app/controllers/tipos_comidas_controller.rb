04a0452b31f60150eeaab66b257aca9dd7e89181
rene rodriguez <reneclavijo@live.com>
2021-10-27 20:36:08 -0500

    agregando el filtro para el controlador de tipos_comidas

--- app/controllers/tipos_comidas_controller.rb
+++ app/controllers/tipos_comidas_controller.rb
@@ -1,6 +1,8 @@
 # Los controladores son en "plural" y hacen referencia al modelo 
 class TiposComidasController < ApplicationController
 
+    before_action :asignar_tipo_comida, only: [:mostrar, :editar, :actualizar, :eliminar]
+
     # GET /tipos_comidas
     def listar
         @todos_los_tipos = TipoComida.all.order(id: :asc)
@@ -16,13 +18,11 @@ class TiposComidasController < ApplicationController
     def mostrar
         # vista para mostrar el detalle de un tipo de comida
         # por ejemplo, podríamos mostrar todos los restaruantes adjuntos
-        @tipo_comida = TipoComida.find(params[:id])
     end
 
     # GET /tipos_comidas/:id/editar
     def editar
         # mostrar el formulario con los datos de un registro para cambiarlos
-        @tipo_comida = TipoComida.find(params[:id])
     end
 
     # POST /tipos_comidas
@@ -39,7 +39,6 @@ class TiposComidasController < ApplicationController
 
     def actualizar
         # encontrar el registro que quiero editar en la BD
-        @tipo_comida = TipoComida.find(params[:id])
         datos_tipo_comida = params.require(:tipo_comida).permit(:tipo)
         # actualizar los campos necesarios
         @tipo_comida.tipo = datos_tipo_comida[:tipo]
@@ -53,9 +52,16 @@ class TiposComidasController < ApplicationController
     def eliminar
         # pasos para eliminar un registro
         # 1. buscar el registro por ID
-        tipo = TipoComida.find(params[:id])
         # 2. Intentar eliminar el registro
-        tipo.destroy
+        @tipo_comida.destroy
         redirect_to tipos_comidas_path
     end
+
+    private # todos los métodos de aquí hacia abajo SON PRIVADOS 🔐
+
+    def asignar_tipo_comida
+        @tipo_comida = TipoComida.find(params[:id])
+    end
+
+
 end
